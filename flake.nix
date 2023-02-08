1c5df980d09d6a6b6d2cbe6581a1b964cc10b9d4
Louis Hurschler <louis.hurschler@gmx.ch>
2023-02-08 17:46:31 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Personal NixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+    #home-manager = {
+    #  url = github:nix-community/home-manager;
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
+  };
+
+  outputs = { self, nixpkgs, home-manager }:
+    let
+      system = "x86_64-linux";
+      user = "louis";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      lib = nixpkgs.lib;
+    in
+    {
+      formatter.${system} = pkgs.nixpkgs-fmt;
+
+      devShell.${system} = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          rnix-lsp
+        ];
+        buildInputs = [ ];
+      };
+
+
+      nixosConfigurations = {
+        louis-hp = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./hardware-configuration.nix
+            ./configuration.nix
+
+            #home-manager.nixosModules.home-manager
+            #{
+            #  home-manager.useGlobalPkgs = true;
+            #  home-manager.useUserPackages = true;
+            #  home-manager.users.${user} = import ./home.nix;
+            #}
+          ];
+        };
+      };
+    };
+}
